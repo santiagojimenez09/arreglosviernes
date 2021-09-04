aa129fde9a6695d38d9fb1d79a87b32794227527
santiagojc <santijc92@gmail.com>
2021-09-03 19:20:48 -0500

    se codifica arreglo en una sola dimension

---
+++ src/arreglos/Arreglos.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arreglos;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author 505
+ */
+public class Arreglos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Scanner entrada=new Scanner(System.in);
+        
+        int [] numeros=new int[4];
+        
+        
+        
+        for(int i=0;i<numeros.length; i++){
+            
+          System.out.println("Digita el dato: "+i);
+          numeros[i]=entrada.nextInt();
+            
+        }
+        for(int i=0;i<numeros.length; i++){
+            
+          System.out.println(numeros[i]);
+         
+            
+        }
+        
+                
+    }
+    
+}
